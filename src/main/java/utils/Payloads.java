17708d3c8f234c05cfa4e8ccb49bcc1d9165a9dd
LakshikaNanayakkara <LakshikaNanayakkara@172.10.100.181>
2022-08-31 12:54:37 +0530

    tax create changes

--- src/main/java/utils/Payloads.java
+++ src/main/java/utils/Payloads.java
@@ -16,17 +16,16 @@ public class Payloads {
 				"}";
 	}
 
- 
 
 	public static String addTax(String code, String taxTypeCode, String taxType, boolean isActive, int countryId,
 			String countryName) {
 		return " {\r\n" + 
 				"  \"taxTypeId\": " +  code + ",\r\n" + 
-				"  \"taxTypeCode\": \" " + taxTypeCode + "\",\r\n" + 
+				"  \"taxTypeCode\": \"" + taxTypeCode + "\",\r\n" + 
 				"  \"taxType\": \"" + taxType + "\",\r\n" + 
 				"  \"isActive\": " + isActive + ",\r\n" + 
 				"  \"countryId\": "+ countryId + ",\r\n" + 
-				"  \"countryName\": \" " + countryName + "\"\r\n" + 
+				"  \"countryName\": \"" + countryName + "\"\r\n" + 
 				"}";
 	}
 
